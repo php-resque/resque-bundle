55d6601b30c798f199492e6642c07e57c0d17d9f
Paul Croker <paul@zomble.com.au>
2016-01-12 22:14:13 +1000

    work on worker start command

--- Command/WorkerStartCommand.php
+++ Command/WorkerStartCommand.php
@@ -2,28 +2,31 @@
 
 namespace Resque\Bundle\ResqueBundle\Command;
 
+use Resque\Component\Core\Foreman;
+use Resque\Component\Queue\WildcardQueue;
 use Resque\Component\Worker\Registry\WorkerRegistryInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class WorkerStartCommand extends Command
 {
     /**
-     * @var WorkerRegistryInterface
+     * @var Foreman
      */
-    protected $workerRegistry;
+    protected $foreman;
 
     /**
      * Constructor.
      *
-     * @param WorkerRegistryInterface $workerRegistry
+     * @param Foreman $foreman
      */
-    public function __construct(WorkerRegistryInterface $workerRegistry)
+    public function __construct(Foreman $foreman)
     {
-        $this->workerRegistry = $workerRegistry;
+        $this->foreman = $foreman;
 
         parent::__construct();
     }
@@ -34,7 +37,10 @@ class WorkerStartCommand extends Command
             ->setName('resque:worker:start')
             ->setDescription('Starts a new worker process on this host')
             ->addArgument('queues', InputArgument::REQUIRED, 'The queue names (in order) the workers will retrieve jobs from')
-            ->addArgument('count', InputArgument::OPTIONAL, 'The number of workers to start', 1);
+            ->addOption('count', null, InputOption::VALUE_OPTIONAL, 'The number of workers to start', 1)
+            ->addOption('skip-prune', null, InputOption::VALUE_OPTIONAL, 'If you want to skip the pruning dead workers', true)
+            ->addOption('wait', null, InputOption::VALUE_OPTIONAL, 'If you want the console command to wait for the workers to exit', false)
+        ;
 //
 //            'app_include' => getenv('APP_INCLUDE'),
 //            'queue_blocking' => (bool)getenv('BLOCKING'),
@@ -48,8 +54,58 @@ class WorkerStartCommand extends Command
     {
         $output = new SymfonyStyle($input, $output);
 
-        $queues = $input->getArgument('queues');
+        $workerCount = $input->getOption('count');
+        $queues = $this->createQueues($input->getArgument('queues'));
 
         $output->write($queues);
+
+        // This method of worker setup requires an array of queues
+        if (!is_array($queues)) {
+            throw new \Exception("Queues not initialized correctly");
+        }
+        $workers = array();
+        for ($i = 0; $i < $workerCount; ++$i) {
+            $worker = $this->workerFactory->createWorker();
+
+            foreach ($queues as $queue) {
+                $worker->addQueue($queue);
+            }
+
+            $workers[] = $worker;
+        }
+
+        $this->foreman->pruneDeadWorkers();
+
+        $this->foreman->work($workers, $input->getOption('wait'));
+
+        echo sprintf(
+            '%d workers attached to the %s queues successfully started.',
+            count($workers),
+            implode($queues, ',')
+        );
+
+        echo sprintf(
+            'Workers (%s)',
+            implode(', ', $workers)
+        );
+    }
+
+    protected function createQueues($queuesArg)
+    {
+        $configQueues = explode(',', $queuesArg);
+
+        $queues = array();
+        if (in_array('*', $configQueues)) {
+            // @todo service this.
+            $wildcard = new WildcardQueue($this->queueRegistry);
+            $queues[] = $wildcard;
+        } else {
+            foreach ($configQueues as $configQueue) {
+                $queues[] = $this->queueRegistry->createQueue($configQueue);
+            }
+        }
+
+        return $queues;
     }
 }
+
